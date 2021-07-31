41aff3f1461329f436ffcf78dfec84bf374595cd
cacuts <84119438+cacuts@users.noreply.github.com>
2021-07-31 16:50:22 +0800

    Delete dist directory

--- dist/static/js/5.53bbcac124337d2ec383.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{HDvf:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("router-view")],1)},staticRenderFns:[]},l=n("VU/8")(null,r,!1,null,null,null);t.default=l.exports}});
-//# sourceMappingURL=5.53bbcac124337d2ec383.js.map
