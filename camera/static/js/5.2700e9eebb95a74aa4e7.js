5d6c8107bb0c7215482b1dd498169fca9fe524ee
cacuts <longgy1001@163.com>
2022-04-19 16:18:45 +0800

    test3 and Img

---
+++ camera/static/js/5.2700e9eebb95a74aa4e7.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{h86F:function(t,e,r){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var s={render:function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticStyle:{width:"200px",height:"200px"}},[e("img",{attrs:{src:this.src,alt:"",srcset:""}})])},staticRenderFns:[]},n=r("VU/8")({data:function(){return{src:""}},created:function(){this.src=this.$route.query.src}},s,!1,null,null,null);e.default=n.exports}});
+//# sourceMappingURL=5.2700e9eebb95a74aa4e7.js.map
