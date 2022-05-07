257e49c2a26464e243630baebdb8436546a6830d
cacuts <longgy1001@163.com>
2022-05-07 11:25:15 +0800

    testImage

---
+++ camera/static/js/6.726b197c48d73caf943b.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{h86F:function(t,e,r){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var s={render:function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticStyle:{width:"200px",height:"200px"}},[e("img",{attrs:{src:this.src,alt:"",srcset:""}})])},staticRenderFns:[]},n=r("VU/8")({data:function(){return{src:""}},created:function(){this.src=this.$route.query.src}},s,!1,null,null,null);e.default=n.exports}});
+//# sourceMappingURL=6.726b197c48d73caf943b.js.map
